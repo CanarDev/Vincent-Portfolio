a9394cad4b55427a382b29cdf4b3539965ff3b5c
CanarDev <vincent.michel@edu.devinci.Fr>
2022-01-09 21:43:53 +0100

    :lipstick: update burger js file into header js file, add new aniation !

--- JS/Burger.js
+++
@@ -1,8 +0,0 @@
-    let burger = document.getElementById('burger');
-    let burgerLinks = document.getElementById('burgerLinks');
-
-    function functionBurger() {
-        burgerLinks.classList.toggle('navOpen')
-    };
-
-    burger.addEventListener('click', functionBurger)
