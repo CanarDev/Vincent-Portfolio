d74567de7ba262b1b7ae0ba160f1fcd7483687e4
CanarDev <vincent.michel@edu.devinci.Fr>
2022-01-11 23:45:17 +0100

    :construction: add new home section and responsive

---
+++ JS/Scroll.js
@@ -0,0 +1,21 @@
+//
+//   var competences = document.getElementById('skills');
+//   var projects = document.getElementById('projects');
+//   var contact = document.getElementById('contact');
+//   let screenH = screen.height;
+//   let x = 0
+//
+//   if (window.pageYOffset > x < screenH){
+//       window.scrollTo(0, screenH)
+//   }else if (window.pageYOffset > (x + screenH) > 2 * screenH){
+//       window.scrollTo(0, (2 *screenH))
+//   }
+
+
+// 
+// 
+// location.hash
+// https://codepen.io/andrejsharapov/pen/xBGKrr
+
+
+// https://stackoverflow.com/questions/19134967/scroll-to-section-by-section
